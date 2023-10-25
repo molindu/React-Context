652c5c033d690e04533c74b2ab0f77d834252e89
Molindu Yasoda <54410056+molindu@users.noreply.github.com>
2023-10-25 23:20:35 +0530

    Update App.tsx

--- src1/App.tsx
+++ src1/App.tsx
@@ -1,18 +1,1 @@
-import React, { useState } from 'react';
-import Login from './screens/Login';
-import Profile from './screens/Profile';
-import { LoginContext } from './Contexts/LoginContext';
 
-export default function App() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [showProfile, setShowProfile] = useState(false);
-    return (
-        <LoginContext.Provider value={{ username, setUsername, password, setPassword, setShowProfile }}>
-            {showProfile ?
-                <Profile /> :
-                <Login />
-            }
-        </LoginContext.Provider>
-    );
-}
